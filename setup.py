92c833b913efdf009da054df019be38ad43367f9
Mario Idival <marioidival@gmail.com>
2014-02-22 17:53:49 -0300

    change version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(root, 'README.txt')) as f:
 
 setup(
     name = "multipserve",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Mario Idival",
     author_email = "marioidival@gmail.com",
     description = ("Script for using with multiples applications Pyramid"),
