05325cd169be07bb0b25ec25120c7c261afbccc9
Mario Idival <marioidival@gmail.com>
2014-02-24 13:07:02 -0300

    edit setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(root, 'README.txt')) as f:
 
 setup(
     name = "multipserve",
-    version = "1.0.0",
+    version = "1.0.1aa",
     author = "Mario Idival",
     author_email = "marioidival@gmail.com",
     description = ("Script for using with multiples applications Pyramid"),
