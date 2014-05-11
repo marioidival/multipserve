f9893579b9092cefe397522dc0d5373894db40d0
Mario Idival <marioidival@gmail.com>
2014-05-11 13:39:42 -0300

    put install requirements in setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,26 @@ root = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(root, 'README.txt')) as f:
     README = f.read()
 
+with open('requirements.txt') as f:
+    require = f.readlines()
 
+with open('tests_requirements.txt') as f:
+    tests_require = f.readlines()
 
 setup(
-    name = "multipserve",
-    version = "1.0.4",
-    author = "Mario Idival",
-    author_email = "marioidival@gmail.com",
-    description = ("Script for using with multiples applications Pyramid"),
-    long_description = README,
-    license = "BSD",
-    keywords = "multipserve pyramid threading",
-    url = "https://github.com/marioidival/multi_pserve",
-    scripts = ['multipserve/bin/multipserver.py'],
+    name="multipserve",
+    version="1.0.4",
+    author="Mario Idival",
+    author_email="marioidival@gmail.com",
+    description=("Script for using with multiples applications Pyramid"),
+    long_description=README,
+    license="BSD",
+    keywords="multipserve pyramid threading",
+    url="https://github.com/marioidival/multi_pserve",
+    scripts=['multipserve/bin/multipserver.py'],
     packages=['multipserve'],
+    install_requires=require,
+    test_require=tests_require,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
